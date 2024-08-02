8f96490d153c15a0a8a43121ce57399bbe5b3f1a
Katerina Deeter <ktdeeter@gmail.com>
2024-08-01 22:18:55 -0700

    Added the bot turn and am currently testing through it

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 def suits():
     return ["\u2663", "\u2665", "\u2666", "\u2660"]
 def ranks():
-    return ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    return ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
 class Card:
 
@@ -72,25 +72,93 @@ class Hand:
         if card in self.hand:
             self.hand.remove(card)
 
+    def addCard(self, card):
+        self.hand.append(card)
+
     def drawCard(self):
         card = card_deck.getCard()
         self.hand.append(card)
         card_deck.removeCard(card)
 
+    def cardCheck(self, card):
+        if card in self.hand:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return ', '.join([str(card) for card in self.hand])
+
+    def __repr__(self):
+        return self.__str__()
+
+
+def checkPairs(player_hand, player_pile):
+    hand = player_hand.getHand()
+    ii = 0
+    while ii < len(hand) - 1:
+        jj = ii + 1
+        while jj < len(hand):
+            if hand[ii].compareRanks(hand[jj]):
+                player_pile.append(hand[ii])
+                player_pile.append(hand[jj])
+                player_hand.removeCard(hand[jj])
+                player_hand.removeCard(hand[ii])
+                i = 0
+                break
+            else:
+                jj += 1
+        else:
+            ii += 1
+
+
+def cardExchange(player_hand, other_player_hand, card):
+    receiving_hand = player_hand.getHand()
+    giving_hand = other_player_hand.getHand()
+    giving_hand.removeCard(card)
+    receiving_hand.addCard(card)
+
+
+def botRequest(player_hand, other_player_hand):
+    receiving_hand = player_hand.getHand()
+    giving_hand = other_player_hand.getHand()
+    card = random.choice(receiving_hand)
+    cardRank = card.getRank()
+    answer = input("Do you have a " + str(cardRank) + "");
+    hasCard = user_hand.cardCheck(card) #This is currently not working
+    if (answer.upper() == "yes".upper()) and hasCard:
+        cardExchange(receiving_hand, giving_hand, card)
+    else:
+        print("  _\n><_>") # todo make a better fish
+        bot_hand.drawCard()
+
+
+def endTurn(turn):
+    if turn == 1:
+        turn = 0
+    else:
+        turn = 1
+
+def botTurn():
+    checkPairs(bot_hand, bot_pile)
+    botRequest(bot_hand, user_hand)
+    checkPairs(bot_hand, bot_pile)
+    endTurn(current_turn)
+
 
 # Startup Tasks
 card_deck = Deck()
-user_hand = Hand()
-bot_hand = Hand()
+user_hand = Hand(7)
+bot_hand = Hand(7)
+user_pile = []
+bot_pile = []
 card_deck.shuffleCards()
+current_turn = 0 #random.choice([0, 1])
 
-# What comes next?
-# 1. Decks and Hands can now be created. This means that the game is ready to be started
-# 2. With this, I need to create the appropriate behaviors. I have objects but what am I going to be doing with them?
-    # Bot Turn
-        #check empty, request card, receive card, check for pairs (twice in a turn), potentially draw, end turn
-    # User Turn
-        #check empty, check pairs, request, receive, check, draw, end turn
+print(user_hand)
+print(bot_hand)
+botTurn()
+print(bot_hand)
 
 # Hearts: \u2665
 # Diamonds: \u2666
